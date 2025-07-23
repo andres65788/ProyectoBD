ec91845d4284b79b1c70962ec904539f9b4eef1f
andre <andresmd519@gmail.com>
2025-07-23 13:50:56 -0600

    llamados de los procedimientos

--- Sentencias.sql
+++ Sentencias.sql
@@ -8,7 +8,7 @@ SELECT
     p.stock,
     c.nombre AS nombre_categoria
 FROM Productos AS p
-JOIN Categorias AS c ON p.id_categoria = c.id_categoria
+ INNER JOIN Categorias AS c ON p.id_categoria = c.id_categoria
 WHERE p.stock > 0
 ORDER BY c.nombre, p.precio;
 
@@ -18,8 +18,9 @@ SELECT
     c.nombre AS nombre_cliente,
     SUM(dp.cantidad * dp.precio_unitario) AS total_compras
 FROM Clientes AS c
-JOIN Pedidos AS p ON c.id_cliente = p.id_cliente
-JOIN Detalles_Pedido AS dp ON p.id_pedido = dp.id_pedido
+INNER JOIN Pedidos AS p ON c.id_cliente = p.id_cliente
+INNER JOIN Detalles_Pedido AS dp ON p.id_pedido = dp.id_pedido
+WHERE p.estado = 'pendiente'
 GROUP BY c.nombre
 ORDER BY total_compras DESC;
 
@@ -29,7 +30,7 @@ SELECT
     p.nombre AS nombre_producto,
     AVG(r.calificacion) AS calificacion_promedio
 FROM Productos AS p
-JOIN Reseñas AS r ON p.id_producto = r.id_producto
+INNER JOIN Reseñas AS r ON p.id_producto = r.id_producto
 GROUP BY p.nombre
 ORDER BY calificacion_promedio DESC
 LIMIT 5;
