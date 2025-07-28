6f16c7e4fc1d14da98bf1463c2fb74ddbf4316a6
Andres65788 <andresmd519@gmail.com>
2025-07-28 10:30:31 -0600

    cositas

--- llamados_procedimentos.sql
+++ llamados_procedimentos.sql
@@ -1,37 +1,26 @@
 
-CALL RegistrarPedido(
-    p_id_cliente = 5,          
-    p_id_producto = 10,        
-    p_cantidad = 2             
-);
-CALL RegistrarReseña(
-    p_id_producto = 15,       
-    p_id_cliente = 3,          
-    p_calificacion = 4,        
-    p_comentario = 'Buen producto, pero el envio tardó' 
-);
-CALL ActualizarStockProducto(
-    p_id_producto = 8,         
-    p_cantidad = 5             
-);
-CALL CambiarEstadoPedido(
-    p_id_pedido = 27,          
-    p_nuevo_estado = 'enviado'
-);
-CALL EliminarReseñasProducto(
-    p_id_producto = 12         
-);
-CALL AgregarNuevoProducto(
-    p_nombre = 'Smartwatch X200',
-    p_descripcion = 'Reloj inteligente con monitor cardiaco',
-    p_precio = 199.99,
-    p_stock = 50,
-    p_id_categoria = 3         
-);
-CALL ActualizarCliente(
-    p_id_cliente = 7,          
-    p_nombre = 'Maria Gonzalez',
-    p_telefono = '5551234567', 
-    p_direccion = 'Calle Nueva 123' 
-);
+USE Tienda_Online;
+
+-- Registrar un nuevo pedido
+CALL RegistrarPedido(5, 10, 2);
+
+-- Registrar una nueva reseña
+CALL RegistrarResena(15, 3, 4, 'Buen producto, pero el envío tardo');
+
+-- Actualizar el stock de un producto
+CALL ActualizarStockProducto(8, 5);
+
+-- Cambiar el estado de un pedido
+CALL CambiarEstadoPedido(27, 'enviado');
+
+-- Eliminar reseñas de un producto
+CALL EliminarReseñasProducto(12);
+
+-- Agregar un nuevo producto
+CALL AgregarNuevoProducto('Smartwatch X200', 'Reloj inteligente con monitor cardiaco', 199.99, 50, 3);
+
+-- Actualizar la información de un cliente
+CALL ActualizarCliente(7, 'Maria Gonzalez', '5551234567', 'Calle Nueva 123');
+
+-- Generar reporte de stock bajo
 CALL ReporteStockBajo();
